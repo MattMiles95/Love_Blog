4595553476020002829e512a538fcbefd995a195
Matt Miles <matt_miles2@live.co.uk>
2024-10-12 07:27:00 +0000

    Add collaboration request form to About app

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-mattmiles95-loveblogs-a6eg74nagq1.ws-eu116.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-mattmiles95-loveblogs-car2cdujjzb.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
@@ -110,7 +110,7 @@ DATABASES = {
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
     "https://*.herokuapp.com",
-    "https://*.8000-mattmiles95-loveblogs-a6eg74nagq1.ws-eu116.gitpod.io"
+    "https://*.8000-mattmiles95-loveblogs-car2cdujjzb.ws.codeinstitute-ide.net"
 ]
 
 # Password validation
