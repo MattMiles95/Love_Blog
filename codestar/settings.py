56dd207b2def4b31bf4183415f0643f028f9496f
Matt Miles <159185894+MattMiles95@users.noreply.github.com>
2024-10-17 16:31:11 +0000

    Remove venv and update DEBUG to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-mattmiles95-loveblogs-car2cdujjzb.ws.codeinstitute-ide.net',
